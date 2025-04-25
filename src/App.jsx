7352bb35c636fb85fd4d98781a17a3a0e0f4df77
Akin Evans <aevans@sageoak.education>
2025-04-25 12:26:00 -0700

    Change titles, add button

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './components/Button';
+function App() {
+  console.log('Hello World');
+
+  return (
+    <>
+      <h1>React Test</h1>
+      <Button name='Alert' />
+    </>
+  );
+}
+
+export default App;
